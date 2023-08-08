1cd07b9a8920b30a32383bc3a2f75a931ec2f635
Samuel Arantes <65547548+Samuel-pixe@users.noreply.github.com>
2023-08-08 13:32:37 -0300

    At.08.08.2023

--- PHP/htdocs/index.php
+++ PHP/htdocs/index.php
@@ -1,20 +1,17 @@
-<!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8">
+		<meta charset="utf-8" />
 		<title>Curso PHP</title>
 	</head>
-
 
 	<body>
 		<?php
 			echo 'Utilizando a tag padrão';
 		?>
-		<br>
-		<?=
-			'Utilizando a tag impressão';
-		?>
+		<br />
+		<?= 'Utilizando a tag impressão' ?>
 
+		<br />
+		<? echo 'Utilizando a tag curta'; ?>
 	</body>
-
 </html>
