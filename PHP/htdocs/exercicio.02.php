73da9590cd329400ef05f0ebd9691158579f3799
Samuel Arantes <65547548+Samuel-pixe@users.noreply.github.com>
2023-08-10 13:32:42 -0300

    At.10.08.2023

---
+++ PHP/htdocs/exercicio.02.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+	<html>
+	
+	<head>
+		<title></title>
+	</head>
+	
+	<body>
+		
+	<?php
+	$idade = 19;
+	$peso =72;
+		
+		if(($idade >= 16 && $idade <= 69) && $peso >= 50) (
+			echo "Atende aos requisitos";
+		else{ 
+			echo "Não atende aos requisitos";
+			} 
+	?>
+	</body>
+	
+	</html>
+	
