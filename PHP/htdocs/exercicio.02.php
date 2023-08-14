f653c467479b697a77bdd1b57423fa2c6595a43f
Samuel Arantes <65547548+Samuel-pixe@users.noreply.github.com>
2023-08-14 13:37:35 -0300

    At,.14.08.2023

--- PHP/htdocs/exercicio.02.php
+++ PHP/htdocs/exercicio.02.php
@@ -8,15 +8,33 @@
 	<body>
 		
 	<?php
-	$idade = 19;
-	$peso =72;
-		
-		if(($idade >= 16 && $idade <= 69) && $peso >= 50) (
-			echo "Atende aos requisitos";
-		else{ 
-			echo "Não atende aos requisitos";
-			} 
-	?>
+		function calcularIRPF($ssalario) { 
+			Simposto = 0;
+			
+			if($salario <= 1903.98) { 
+				$imposto = O;
+			}
+			
+			else if($salario >= 1903.99 && $salario <= 2826.65) { 
+				$imposto = ($salario * 7.5) / 100;
+			}
+			
+			else if($salario >= 2826.66 && $salario <= 3751.05) {
+				$imposto = ($salario * 15) / 100;
+			}
+			
+			else if($salario >= 3751.06 && $salario <= 4664.08) {
+				$imposto = ($salario * 22.5) / 100;	
+			}
+			else{ 
+			Simposto = ($salario * 27.5) / 100;
+			}
+		return $imposto;
+		}
+
+		//chama/imprime o retorno da função passando o salário por parâmetro
+		echo calcularIRPF(5000);
+		?>
 	</body>
 	
 	</html>
